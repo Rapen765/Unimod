e14f06ea95ca20b22f727481ca2a46fa49cfec43
Raxeli1 <rapen2011@gmail.com>
2023-06-04 07:48:47 +0200

    Fixed inventory

---
+++ src/Assets/Scripts/UI/Level Scene/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    int inventory = 0;
+    public GameObject[] gameObjects;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            if (inventory>0)
+               inventory -= 1;
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (inventory < gameObjects.Length)
+                inventory += 1;
+        }
+        for(int i=0;i<gameObjects.Length;i++)
+        {
+            if(inventory == i)
+            {
+                gameObjects[i].SetActive(true);
+            }
+            else
+            {
+                gameObjects[i].SetActive(false);
+            }
+        }
+    }
+}
